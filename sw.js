3fad8256dfb659d77ce6f8bf32ce5704ccd4c419
zengzhihui <418116135@qq.com>
2018-11-26 10:12:17 +0800

    初始化个人主页仓库

---
+++ sw.js
@@ -0,0 +1,169 @@
+var cacheName = 'zzh';     // 缓存的名称  
+// install 事件，它发生在浏览器安装并注册 Service Worker 时        
+self.addEventListener('install', event => { 
+/* event.waitUtil 用于在安装成功之前执行一些预装逻辑
+ 但是建议只做一些轻量级和非常重要资源的缓存，减少安装失败的概率
+ 安装成功后 ServiceWorker 状态会从 installing 变为 installed */
+  
+    console.log('service worker: install');
+
+  // cache core files
+  event.waitUntil(
+    installStaticFiles()
+    .then(() => self.skipWaiting())
+  );
+});
+  
+
+
+
+// configuration
+`use strict`;
+
+const
+  version = '1.0.0',
+  CACHE = version + '::PWAsite',
+  offlineURL = '/offline/',
+  installFilesEssential = [
+    '/',
+    '/manifest.json',
+    '/css/styles.css',
+    '/js/main.js',
+    '/js/offlinepage.js',
+    '/images/logo/logo152.png'
+  ].concat(offlineURL),
+  installFilesDesirable = [
+    '/favicon.ico',
+    '/images/logo/logo016.png',
+    '/images/hero/power-pv.jpg',
+    '/images/hero/power-lo.jpg',
+    '/images/hero/power-hi.jpg'
+  ];
+
+// install static assets
+function installStaticFiles() {
+
+  return caches.open(CACHE)
+    .then(cache => {
+
+      // cache desirable files
+      cache.addAll(installFilesDesirable);// 如果所有的文件都成功缓存了，便会安装完成。如果任何文件下载失败了，那么安装过程也会随之失败。
+
+      // cache essential files
+      return cache.addAll(installFilesEssential);
+
+    });
+
+}
+
+// clear old caches
+function clearOldCaches() {
+
+  return caches.keys()
+    .then(keylist => {
+
+      return Promise.all(
+        keylist
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      );
+
+    });
+
+}
+
+
+
+
+// application activated
+self.addEventListener('activate', event => {
+
+  console.log('service worker: activate');
+
+	// delete old caches
+  event.waitUntil(
+    clearOldCaches()
+    .then(() => self.clients.claim())
+	);
+
+});
+
+
+// is image URL?
+let iExt = ['png', 'jpg', 'jpeg', 'gif', 'webp', 'bmp'].map(f => '.' + f);
+function isImage(url) {
+
+  return iExt.reduce((ret, ext) => ret || url.endsWith(ext), false);
+
+}
+
+
+// return offline asset
+function offlineAsset(url) {
+
+  if (isImage(url)) {
+
+    // return image
+    return new Response(
+      '<svg role="img" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title>offline</title><path d="M0 0h400v300H0z" fill="#eee" /><text x="200" y="150" text-anchor="middle" dominant-baseline="middle" font-family="sans-serif" font-size="50" fill="#ccc">offline</text></svg>',
+      { headers: {
+        'Content-Type': 'image/svg+xml',
+        'Cache-Control': 'no-store'
+      }}
+    );
+
+  }
+  else {
+
+    // return page
+    return caches.match(offlineURL);
+
+  }
+
+}
+
+/**
+为 fetch 事件添加一个事件监听器。接下来，使用 caches.match() 函数来检查传入的请求 URL 是否匹配当前缓存中存在的任何内容。如果存在的话，返回缓存的资源。
+如果资源并不存在于缓存当中，通过网络来获取资源，并将获取到的资源添加到缓存中。
+*/
+// application fetch network data
+self.addEventListener('fetch', event => {
+
+  // abandon non-GET requests
+  if (event.request.method !== 'GET') return;
+
+  let url = event.request.url;
+
+  event.respondWith(
+
+    caches.open(CACHE)
+      .then(cache => {
+
+        return cache.match(event.request)
+          .then(response => {
+
+            if (response) {
+              // return cached file
+              console.log('cache fetch: ' + url);
+              return response;
+            }
+
+            // make network request
+            return fetch(event.request)
+              .then(newreq => {
+
+                console.log('network fetch: ' + url);
+                if (newreq.ok) cache.put(event.request, newreq.clone());
+                return newreq;
+
+              })
+              // app is offline
+              .catch(() => offlineAsset(url));
+
+          });
+
+      })
+
+  );
+
+});
